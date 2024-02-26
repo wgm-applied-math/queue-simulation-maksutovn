1a0e01886fce9e8e956557873753728c73fa6a26
maksutovn <156250496+maksutovn@users.noreply.github.com>
2024-02-26 16:38:53 -0500

    inserting calculations

--- run_ServiceQueue.m
+++ run_ServiceQueue.m
@@ -51,31 +51,41 @@ NInSystem = vertcat(NInSystemSamples{:});
 % Start with a histogram.  The result is an empirical PDF, that is, the
 % area of the bar at horizontal index n is proportional to the fraction of
 % samples for which there were n customers in the system.
-h = histogram(NInSystem, Normalization="probability", BinMethod="integers");
+h = histogram(ax,NInSystem, Normalization="probability", BinMethod="integers");
 
 % MATLAB-ism: Once you've created a picture, you can use "hold on" to cause
 % further plotting function to work with the same picture rather than
 % create a new one.
-hold on;
+hold (ax,"on");
 
 % For comparison, plot the theoretical results for a M/M/1 queue.
 % The agreement isn't all that good unless you run for a long time, say
 % max_time = 10,000 units, and LogInterval is large, say 10.
-rho = q.ArrivalRate / q.DepartureRate;
-P0 = 1 - rho;
+rho1 = q.ArrivalRate / q.DepartureRate;
+rho2 = q.ArrivalRate / q.DepartureRateHelper;
+P0 = 1 - rho1;
 nMax = 10;
 ns = 0:nMax;
+wh = 0:nMax;
 P = zeros([1, nMax+1]);
 P(1) = P0;
+P2(1) = P0;
 for n = 1:nMax
-    P(1+n) = P0 * rho^n;
+    P(1+n) = P0 * rho1^n;
 end
 plot(ns, P, 'o', MarkerEdgeColor='k', MarkerFaceColor='r');
+for n = 1:nMax
+    P2(1+n) = P0 * rho1 * rho2^(n-1);
+end
+plot(wh, P2, 'o', MarkerEdgeColor='k', MarkerFaceColor='b');
 
 % This sets some paper-related properties of the figure so that you can
 % save it as a PDF and it doesn't fill a whole page.
 % gcf is "get current figure handle"
 % See https://stackoverflow.com/a/18868933/2407278
+fig = figure();
+t = tiledlayout(fig, 1, 1);
+ax = nexttile(t);
 fig = gcf;
 fig.Units = 'inches';
 screenposition = fig.Position;
